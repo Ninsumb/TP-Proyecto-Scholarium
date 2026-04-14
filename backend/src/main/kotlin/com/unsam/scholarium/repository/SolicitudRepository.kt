1b721065ed65c5fbc36d7349e1287d114dc509b6
CaroVillalba <carovillalba012@gmail.com>
2026-04-14 17:57:37 -0300

    Feat: SolicitudRepository

---
+++ backend/src/main/kotlin/com/unsam/scholarium/repository/SolicitudRepository.kt
@@ -0,0 +1,15 @@
+package com.unsam.scholarium.repository
+
+import com.unsam.scholarium.model.Solicitud
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+
+@Repository
+interface SolicitudRepository : JpaRepository<Solicitud, Long> {
+    fun findByUsuarioId(usuarioId: Long): List<Solicitud>
+
+    fun findByPortalId(portalId: Long): List<Solicitud>
+
+    fun findByEstadoAndPortalId(estado: Estado, portalId: Long): List<Solicitud>
+}
