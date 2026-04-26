3b33b60d77034adcf5f8d73d34eb2ebc9d041b3b
Dawn Harper <salguerovalentino@hotmail.com>
2026-04-26 20:53:17 -0300

    queries para contar

--- backend/src/main/kotlin/com/unsam/scholarium/repository/MaterialRepository.kt
+++ backend/src/main/kotlin/com/unsam/scholarium/repository/MaterialRepository.kt
@@ -1,10 +1,15 @@
 package com.unsam.scholarium.repository
 
+import com.unsam.scholarium.model.Material
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-
+import java.util.UUID
 
 @Repository
-interface MaterialRepository : JpaRepository<Material, Long> {
-    fun findByMateriaId(materiaId: Long): List<Material>
+interface MaterialRepository : JpaRepository<Material, UUID> {
+    fun findByIdIn(ids: List<UUID>): List<Material>
+
+    @Query("SELECT COUNT(m) FROM Material m WHERE m.materia.carpeta.portal.id = :portalId")
+    fun countByPortalId(portalId: Long): Int
 }
