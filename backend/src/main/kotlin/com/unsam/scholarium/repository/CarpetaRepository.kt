9da14ca8b745ae79584a02e4261ea78cf09f417f
Dawn Harper <salguerovalentino@hotmail.com>
2026-04-26 20:52:34 -0300

    arreglos para que levante el back

--- backend/src/main/kotlin/com/unsam/scholarium/repository/CarpetaRepository.kt
+++ backend/src/main/kotlin/com/unsam/scholarium/repository/CarpetaRepository.kt
@@ -1,11 +1,13 @@
 package com.unsam.scholarium.repository
 
+import com.unsam.scholarium.model.Carpeta
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
 interface CarpetaRepository : JpaRepository<Carpeta, UUID> {
-    fun findByPortalId(portalId: UUID): List<Carpeta>
+    fun findByPortalId(portalId: Long): List<Carpeta>
     fun findByCarpetaPadreId(carpetaPadreId: UUID): List<Carpeta>
-    fun findByPortalIdAndCarpetaPadreId(portalId: UUID, carpetaPadreId: UUID): List<Carpeta>
+    fun findByPortalIdAndCarpetaPadreId(portalId: Long, carpetaPadreId: UUID): List<Carpeta>
 }
