81cee6287d2ca3bd86cda0c34fd69dc7bc19937a
Ninsumb <lambdadeltabern@gmail.com>
2026-05-12 20:45:42 -0300

    [BACKEND] Se agrega Service que se me olvido xd

--- frontend/src/main/services/Portal/PortalService.ts
+++ frontend/src/main/services/Portal/PortalService.ts
@@ -1,11 +1,10 @@
-// src/services/PortalService.ts
 import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 import type { PortalDetailResponse } from '../../types/Portal/Portal';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:9001/api';
 
-// Tipos para los bloques (ajústalos según tu BlockComponents)
+
 export interface Block {
   id: string;
   type: string;
@@ -45,7 +44,6 @@ class PortalService {
       (response) => response,
       (error) => {
         if (error.response?.status === 401) {
-          // Token inválido o expirado
           localStorage.removeItem('token');
           localStorage.removeItem('userEmail');
           localStorage.removeItem('userName');
@@ -61,37 +59,6 @@ class PortalService {
     const response = await this.api.get<PortalDetailResponse>(`/${portalId}`);
     return response.data;
   }
-
-
-/*   async getPortalBloques(portalId: number): Promise<Block[]> {
-    try {
-      const response = await this.api.get<BloqueBackendResponse[]>(`/${portalId}/bloques`);
-      
-     
-      return response.data.map(bloque => ({
-        id: bloque.id,
-        type: bloque.type,
-        data: bloque.data
-      }));
-    } catch (error: any) {
-      
-      if (error.response?.status === 404) {
-        return [];
-      }
-      throw error;
-    }
-  }
-
- 
-  async savePortalBloques(portalId: number, bloques: Block[]): Promise<void> {
-    const bloquesRequest = bloques.map(bloque => ({
-      id: bloque.id,
-      type: bloque.type,
-      data: bloque.data
-    }));
-
-    await this.api.put(`/${portalId}/bloques`, bloquesRequest);
-  } */
 }
 
 export const portalService = new PortalService();
