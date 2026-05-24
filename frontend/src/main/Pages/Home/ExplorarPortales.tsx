42aa3328519896d0b48b960a881f4208ab36cec5
CaroVillalba <carovillalba012@gmail.com>
2026-05-24 19:04:21 -0300

    fix: lista de portales si viene vacia no muestra nada

--- frontend/src/main/Pages/Home/ExplorarPortales.tsx
+++ frontend/src/main/Pages/Home/ExplorarPortales.tsx
@@ -64,7 +64,7 @@ export function ExplorePortals() {
     return null;
   };
 
-  const filteredPortals = portales.filter(portal =>
+  const filteredPortals = (portales ?? []).filter(portal =>
     portal.nombre.toLowerCase().includes(searchQuery.toLowerCase()) ||
     portal.descripcion.toLowerCase().includes(searchQuery.toLowerCase())
   );
